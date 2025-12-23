de6bf6d38feaeee0c41f238872740f5186bfeab4
jpledezma <jpledezma01@outlook.com>
2025-12-22 22:47:19 -0300

    feat: update default-tab component styles

--- src/app/components/default-tab/default-tab.component.ts
+++ src/app/components/default-tab/default-tab.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  IonTitle,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonButton,
+  ModalController,
+} from '@ionic/angular/standalone';
+import { AddListComponent } from '../add-list/add-list.component';
 
 @Component({
   selector: 'app-default-tab',
   templateUrl: './default-tab.component.html',
   styleUrls: ['./default-tab.component.scss'],
+  imports: [IonButton, IonContent, IonToolbar, IonHeader, IonTitle],
 })
-export class DefaultTabComponent  implements OnInit {
+export class DefaultTabComponent {
+  modalCtrl = inject(ModalController);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  async openAddListForm() {
+    const addItemFormModal = await this.modalCtrl.create({
+      component: AddListComponent,
+    });
 
+    addItemFormModal.present();
+  }
 }
